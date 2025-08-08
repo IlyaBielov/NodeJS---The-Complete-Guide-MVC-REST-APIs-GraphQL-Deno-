8079e900eb3fe3bd902940529bea4a4f594aacc1
Ilya Bielov <ibioriginalibi@gmail.com>
2025-08-08 18:30:30 +0300

    Add MVC structure with models, views, and controllers for product management

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require("path");
 
+const errorController = require('./controllers/error');
+
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const rootDir = require('./utils/path');
 
@@ -15,9 +20,6 @@ app.use(express.static(path.join(rootDir, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', 'page-not-found.html'));
-})
+app.use(errorController.get404)
 
 app.listen(3000);
-
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,10 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../utils/path');
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
-
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+router.get('/add-product', productController.getAddProduct);
+router.post('/add-product', productController.postAddProduct)
 
 module.exports = router;
